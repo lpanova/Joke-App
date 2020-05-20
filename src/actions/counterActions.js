1622fd8a7d17ff37ca45ae6e45d8c4e3df785fa3
liliapanova <lilia_panova@abv.bg>
2020-05-20 13:54:58 +0300

    Added new functionality

--- src/actions/counterActions.js
+++ src/actions/counterActions.js
@@ -5,3 +5,13 @@ export const increment = () => ({
 export const decrement = () => ({
   type: 'DECREMENT'
 });
+
+export const incrementWithValue = (number) => ({
+  type: 'INCREMENT_WITH_VALUE',
+  payload: { number }
+});
+
+export const decrementWithValue = (number) => ({
+  type: 'DECREMENT_WITH_VALUE',
+  payload: { number }
+});
