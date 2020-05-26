058f3fb8d948d06af772e91db228d64b5a7680d2
liliapanova <lilia_panova@abv.bg>
2020-05-26 12:54:45 +0300

    Integrated redux-logger  and modified joke component to use redux

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
